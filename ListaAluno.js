cd2e6005eca59f29375e17f389300120b19574aa
Yuri Aleixo <yuriantonov@yahoo.com.br>
2022-08-03 17:00:46 -0300

    Lista Aluno

---
+++ ListaAluno.js
@@ -0,0 +1,14 @@
+// Se o numero for par , escreva par, e o numero correspondente
+// Se o numero for impar, escreva impar e o numero correspondete
+// Se o numero for zero, e o numero correspondente 
+
+let nAluno = 10
+
+for( var contador =0; contador<nAluno; contador++){
+
+if ( contador ==0){console.log("Contador zero")}
+if (contador %2 ==0) {console.log(contador,"Número par")} else{console.log(contador,"Número Impar")}
+}
+console.log(contador)
+
+// nos siga para mais dicas de programação !!! deixa seu Like
